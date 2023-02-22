c7dffd5a16b42c84ef1c35cbe67a64f419da0ff1
devvspaces <netrobepy@gmail.com>
2023-02-22 12:37:15 +0100

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<unistd.h>
+// #include <unistd.h>
 
 /**
  * main - Entry point
@@ -7,7 +7,7 @@
  *    ssize_t write(int fd, const void *buf, size_t count);
  *
  * Return: This time we return an error 1.
-*/
+ */
 
 int main(void)
 {
