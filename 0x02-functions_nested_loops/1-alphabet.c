c3d4a2146a1721ec405155ec4021b7506e3c153c
devvspaces <netrobepy@gmail.com>
2023-02-23 05:28:21 +0100

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - print a - z
+ * 
+ * Description: loop through a - z
+ */
+void print_alphabet(void)
+{
+	char a = 'a';
+	
+	for (; a < 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
