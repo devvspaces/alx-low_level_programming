b449ac9251febeddcf8b4eca7fd150e00b751a79
devvspaces <netrobepy@gmail.com>
2023-02-23 05:33:18 +0100

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a - z 10x
+ *
+ * Description: loop through a - z
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		a = 'a';
+
+		for (; a <= 'z'; a++)
+			_putchar(a);
+	}
+
+	_putchar('\n');
+}
