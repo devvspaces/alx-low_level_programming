8805f56c8691200cbd271df59335d797a083ff56
devvspaces <netrobepy@gmail.com>
2023-02-25 05:50:49 +0100

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,14 +13,12 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	else
+
+	for (postn = 1; postn <= n; ++postn)
 	{
-		for (postn = 1; postn <= n; ++postn)
-		{
-			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('\n');
-		}
+		for (space = 1; space < postn; ++space)
+			_putchar(' ');
+		_putchar(92); /*is equal to '/' char*/
+		_putchar('\n');
 	}
 }
