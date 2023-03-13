afe1c55199821eaf11571663fe88fb14f71c482b
devvspaces <netrobepy@gmail.com>
2023-03-13 06:22:43 +0100

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,21 +1,15 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * main - sweet main function
- *
- * @argc: number of arguments passed to command line
- * @argv: array of arguments as strings passed to command line
- *
- * Description: Prints argv
- *
+ * main - prints program name, followed by a new line
+ * @argc: int
+ * @argv: list
  * Return: 0
  */
-int main(int argc, char **argv)
+
+int main(int argc, char const *argv[])
 {
 	(void)argc;
-
 	printf("%s\n", argv[0]);
-
 	return (0);
 }
