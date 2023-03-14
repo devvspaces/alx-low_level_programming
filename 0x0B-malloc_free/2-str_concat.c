4c3b65c3cbf0384323abb8c2c13d6504fe4064b2
devvspaces <netrobepy@gmail.com>
2023-03-14 07:16:13 +0100

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+ * *str_concat - The returned pointer should point to a
+ * newly allocated space in memory which contains the
+ * contents of s1, followed by the contents of s2, and
+ * null terminated
+ *
+ * @s1: string
+ * @s2: string
+ *
+ * Return: 0
  */
-
-
-int _strlen(char *s)
-{
-int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
-}
-
-/**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: pointer
- */
-
 char *str_concat(char *s1, char *s2)
 {
-int size1, size2, i;
-char *m;
+	char *s;
+	int i = 0, size1 = 0, size2 = 0;
 
-if (s1 == NULL)
-	s1 = "\0";
-if (s2 == NULL)
-	s2 = "\0";
+	if (s1 == NULL)
+		s1 = "";
 
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-m = malloc((size1 + size2) *sizeof(char) + 1);
-if (m == 0)
-	return (0);
+	if (s2 == NULL)
+		s2 = "";
 
-for (i = 0; i <= size1 + size2; i++)
-{
-	if (i < size1)
-		m[i] = s1[i];
-	else
-		m[i] = s2[i - size1];
-}
-m[i] = '\0';
-return (m);
+	for (; s1[size1]; size1++)
+		;
+
+	for (; s2[size2]; size2++)
+		;
+
+	s = malloc((size1 + size2) * sizeof(*s1) + 1);
+
+	for (; i < (size1 + size2); i++)
+	{
+		if ((size1 - 1) < i)
+			s[i] = s2[i - size1];
+		else
+			s[i] = s1[i];
+	}
+
+	s[i] = '\0';
+
+	return (s);
 }
