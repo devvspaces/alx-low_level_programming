a44f14afc02702f8e4d9ed41c38a4d1fff8e2650
devvspaces <netrobepy@gmail.com>
2023-03-18 10:05:08 +0100

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,14 +1,39 @@
 #include "main.h"
 
+/**
+ * mul - Multiply a and b
+ *
+ * Description: Performs Overflow and zero checks
+ *
+ * @a: first value
+ * @b: second value
+ *
+ * Return: multiplication result or -1 when it
+ * fails check
+ */
 int mul(unsigned int a, unsigned int b)
 {
 	unsigned int x = a * b;
-	if ((a != 0 && x / a != b) || (a == 0) || (b == 0)) {
+
+	if ((a != 0 && x / a != b) || (a == 0) || (b == 0))
+	{
 		return (-1);
 	}
 	return (x);
 }
 
+/**
+ * _calloc - allocates memory for an
+ * array of nmemb of size bytes each
+ *
+ * Description: Check for integer overflow
+ *
+ * @nmemb: amount of contiguous memory blocks
+ * @size: size of each block in bytes
+ *
+ * Return: memory address if success or
+ * NULL if failed
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int res = mul(nmemb, size);
@@ -16,12 +41,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (res == -1)
 		return (NULL);
-	
+
 	mem = malloc(res);
 
 	if (mem == NULL)
 		return (NULL);
-	
+
 	*mem = 0;
 
 	return (mem);
