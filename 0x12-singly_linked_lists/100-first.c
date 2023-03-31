b6759c7f7426cf5e8856559a45e0a827e048c8da
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-03-31 05:08:05 +0100

    Completed task advanced 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void before_func(void) __attribute__((constructor));
+
+/**
+ * before_func - print text before
+ * the main function
+ */
+void before_func(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
