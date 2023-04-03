e4b1810a7685bb95f3fa9200260621d52c35a92a
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-03 22:31:03 +0100

    completed and tested task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all the data (n)
+ *
+ * @head: node
+ *
+ * Return: the sum
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *point = head;
+	int sum = 0;
+
+	while (point != NULL)
+	{
+		if (head->next == NULL)
+			break;
+
+		sum += point->n;
+		point = point->next;
+	}
+
+	return (sum);
+}
