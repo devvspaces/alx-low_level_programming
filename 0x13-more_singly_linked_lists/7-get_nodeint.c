1c04db1c8decc977236b42efa50cfc1e143e503a
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-03 22:24:50 +0100

    completed getter

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of
+ * a listint_t linked list.
+ *
+ * @head: node
+ * @index: index of the node to return
+ *
+ * Return: the nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *point = head;
+
+	while (index > 0)
+	{
+		if (head->next == NULL)
+			return (NULL);
+		index--;
+		point = head->next;
+	}
+
+	return (point);
+}
