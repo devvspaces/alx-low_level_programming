f48b5aebf4c71405bfea2d06b4184b9be822c2c9
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-03 21:59:11 +0100

    completed and solved 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free linked list
+ *
+ * @head: node
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+
+	if ((*head)->next == NULL)
+	{
+		free(*head);
+		return;
+	}
+
+	free_listint2(&(*head)->next);
+	free(*head);
+	*head = NULL;
+}
