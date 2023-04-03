cc2245b0770e97d8e9b9354884c0ea1bb46e0810
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-03 21:31:48 +0100

    completed task 3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * find_tail - find the tail of the
+ * linked list.
+ *
+ * @node: node
+ *
+ * Return: address of the tail node
+ */
+listint_t *find_tail(listint_t *node)
+{
+	if (node->next == NULL)
+		return (node);
+
+	return (find_tail(node->next));
+}
+
+/**
+ * add_node_end - add a new node to the
+ * linked list (append)
+ *
+ * @head: node
+ * @str: string
+ *
+ * Return: address of the new node
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (*head != NULL)
+		find_tail(*head)->next = new;
+	else
+		*head = new;
+
+	return (new);
+}
