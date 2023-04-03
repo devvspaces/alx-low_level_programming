5c7e00b3bc7dd331398602ca21d5efca70018051
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-03 22:18:22 +0100

    added docs to task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * pop_listint - pop node in linked list
+ *
+ * @head: node
+ *
+ * Return: value of popped node
+ */
 int pop_listint(listint_t **head)
 {
 	int data = 0;
