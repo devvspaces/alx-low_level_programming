76afc41de0d111fac6851cb1ef87b3c4d231519c
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-04 20:05:20 +0100

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,21 +1,63 @@
 #include "lists.h"
 
 /**
+ * detectLoop - print nodes recursively
+ *
+ * @head: node
+ *
+ * Return: count
+ */
+const listint_t *detectLoop(const listint_t *head)
+{
+	const listint_t *slow = head, *fast = head;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	slow = slow->next;
+	fast = fast->next->next;
+
+	while (fast && fast->next)
+	{
+		if (slow == fast)
+			break;
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	if (slow != fast)
+		return (NULL);
+
+	return (slow->next);
+}
+
+/**
  * print_nodes_safe - print nodes recursively
  *
  * @node: node
  * @count: current count
+ * @loop: loop node
  *
  * Return: count
  */
-size_t print_nodes_safe(const listint_t *node, size_t count)
+size_t print_nodes_safe(
+	const listint_t *node,
+	size_t count, const listint_t *loop
+)
 {
 	if (node == NULL)
 		return (count);
 
+	if (node == loop)
+	{
+		printf("[%p] %d\n", (void *)node, node->n);
+		printf("-> [%p] %d\n", (void *)node->next, node->next->n);
+		return (++count);
+	}
+
 	printf("[%p] %d\n", (void *)node, node->n);
 
-	return (print_nodes_safe(node->next, ++count));
+	return (print_nodes_safe(node->next, ++count, loop));
 }
 
 /**
@@ -30,5 +72,5 @@ size_t print_listint_safe(const listint_t *head)
 	if (head == NULL)
 		exit(98);
 
-	return (print_nodes_safe(head, 0));
+	return (print_nodes_safe(head, 0, detectLoop(head)));
 }
