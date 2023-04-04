0ffc6823394725ec1d9dd14de74362904c9c14fe
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-04 13:33:17 +0100

    debugged task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -16,6 +16,7 @@ size_t print_nodes_safe(const listint_t *node, size_t count)
 	if ((node->next != NULL) && (node->next > node))
 	{
 		printf("[%p] %d\n", (void *)node, node->n);
+		printf("-> [%p] %d\n", (void *)node->next, node->next->n);
 		return (++count);
 	}
 
