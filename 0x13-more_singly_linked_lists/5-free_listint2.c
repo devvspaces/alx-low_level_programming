df55fc73d27d2af509224fce8411076f5d976f0a
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-04 18:39:31 +0100

    updated the bug

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@ void free_listint2(listint_t **head)
 {
 	if (*head == NULL)
 	{
-		free(*head);
 		*head = NULL;
+		free(*head);
 		return;
 	}
 
 	free_listint2(&(*head)->next);
-	free(*head);
 	*head = NULL;
+	free(*head);
 }
