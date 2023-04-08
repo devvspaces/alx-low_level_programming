36d6847623b54413c0cd40cdea42fd8691c5c951
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-08 02:35:59 +0100

    debugged task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,28 @@
 #include "main.h"
 
 /**
+ * _power - a ^ b
+ *
+ * @a: a
+ * @b: b
+ *
+ * Return: a raise to power b
+ */
+unsigned long int _power(unsigned long int a, unsigned long int b)
+{
+	unsigned long int i;
+	unsigned long int total = 1;
+
+	if (b == 0)
+		return (1);
+
+	for (i = 0; i < b; i++)
+		total *= a;
+
+	return (total);
+}
+
+/**
  * set_bit - sets the value of a
  * bit to 1 at a given index
  *
@@ -19,7 +41,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	for (; (int)i >= 0; i--)
 		if ((((*n & (1 << i)) >> i) == 1) || (i == index))
-			sum += (1 * (unsigned long int)_pow(2, i));
+			sum += (1 * _power(2, i));
 
 	*n = sum;
 	return (1);
