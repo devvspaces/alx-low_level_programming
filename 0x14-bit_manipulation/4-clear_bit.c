ae4c01f8e2080a56809764966ffeddf2552f2a90
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-08 02:45:01 +0100

    debugged task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,17 +14,12 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned int i = sizeof(unsigned long int) * 8;
 	unsigned long int sum = 0;
 
-	if (index >= i)
+	if (index > i)
 		return (-1);
 
 	for (; (int)i >= 0; i--)
-		if ((((*n & (1 << i)) >> i) == 1))
-		{
-			if (i == index)
-				sum += 0;
-			else
-				sum += (1 * _power(2, i));
-		}
+		if ((((*n & (1 << i)) >> i) == 1) && (i != index))
+			sum += (1 * _power(2, i));
 
 	*n = sum;
 	return (1);
