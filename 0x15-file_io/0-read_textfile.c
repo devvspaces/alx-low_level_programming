fefa16ad65e67f7f1c8fc303b56c9df3ed9f5c64
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-08 17:27:29 +0100

    Completed task cp and debugged task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,20 @@
 #include "main.h"
 
 /**
- * read_textfile -  reads a text file
+ * _free - free buffer
+ *
+ * @p: pointer to buffer
+ *
+ * Return: 0
+ */
+int _free(char **p)
+{
+	free(*p);
+	return (0);
+}
+
+/**
+ * read_textfile - reads a text file
  * and prints it to the POSIX standard output.
  *
  * @filename: filename
@@ -12,38 +25,31 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t stat;
-	ssize_t write_stat;
+	ssize_t stat, write_stat, c_stat;
 	char *buf;
 
 	if ((filename == NULL) || (letters == 0))
 		return (0);
 
 	fd = open(filename, O_RDONLY);
-
 	if (fd < 0)
 		return (0);
 
 	buf = malloc(letters);
 	if (buf == NULL)
-	{
-		free(buf);
-		return (0);
-	}
+		return (_free(&buf));
 
 	stat = read(fd, buf, letters);
 	if (stat < 0)
-	{
-		free(buf);
-		return (0);
-	}
+		return (_free(&buf));
+
+	c_stat = close(fd);
+	if (c_stat < 0)
+		return (_free(&buf));
 
 	write_stat = write(STDOUT_FILENO, buf, stat);
 	if (write_stat < 0 || write_stat < stat)
-	{
-		free(buf);
-		return (0);
-	}
+		return (_free(&buf));
 
 	free(buf);
 	return (write_stat);
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _close_fd - close a file
+ * descriptor
+ *
+ * @fd: file descriptor
+ */
+void _close_fd(int fd)
+{
+	ssize_t c_stat;
+
+	c_stat = close(fd);
+	if (c_stat < 0)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * _check_read - check if a file
+ * descriptor or read is success
+ *
+ * @fd: file descriptor or read byte count
+ * @file: file_from name
+ */
+void _check_read(ssize_t fd, char *file)
+{
+	if (fd < 0)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file);
+		_close_fd(fd);
+		exit(98);
+	}
+}
+
+/**
+ * _check_write - check if a file
+ * descriptor or write is success
+ *
+ * @fd: file descriptor or written byte count
+ * @file: file_to name
+ */
+void _check_write(ssize_t fd, char *file)
+{
+	if (fd < 0)
+	{
+		dprintf(2, "Error: Can't write to file %s\n", file);
+		_close_fd(fd);
+		exit(99);
+	}
+}
+
+/**
+ * _check_and_set - check if args
+ * supplied to cp is valid, if true
+ * set file names for a and b, else
+ * exit with status 97.
+ *
+ * @ac: no of args
+ * @av: args
+ * @a: pointer to file_from
+ * @b: pointer to file_to
+ */
+void _check_and_set(int ac, char **av, char **a, char **b)
+{
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		exit(97);
+	}
+	*a = av[1];
+	*b = av[2];
+}
+
+/**
+ * cp - check the code
+ *
+ * @ac: no of args
+ * @av: args
+ */
+void cp(int ac, char **av)
+{
+	int fd_r, fd_w;
+	char *file_from, *file_to, *buf;
+	ssize_t r_stat, w_stat;
+	size_t read_bytes = 1024;
+
+	_check_and_set(ac, av, &file_from, &file_to);
+
+	fd_r = open(file_from, O_RDONLY);
+	_check_read(fd_r, file_from);
+	fd_w = open(file_to, O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	_check_write(fd_w, file_to);
+
+	while (1)
+	{
+		/* Create a new buffer */
+		buf = malloc(read_bytes);
+		if (buf == NULL)
+			free(buf);
+
+		/* Read 1024 bytes into buffer */
+		r_stat = read(fd_r, buf, read_bytes);
+		if (r_stat < 0)
+			free(buf);
+		_check_read(fd_r, file_from);
+
+		if (r_stat == 0)
+			break;
+
+		/* Write buffer into fd_w */
+		w_stat = write(fd_w, buf, r_stat);
+		if (w_stat < r_stat)
+			free(buf);
+		_check_write(fd_w, file_to);
+
+		free(buf);
+	}
+	free(buf);
+	_close_fd(fd_r);
+	_close_fd(fd_w);
+}
