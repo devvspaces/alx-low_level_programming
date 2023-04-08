448f3673cda5ac4ecfe8295486f3e2f5dbd345e1
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-08 15:13:45 +0100

    completed task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file
+ * and prints it to the POSIX standard output.
+ *
+ * @filename: filename
+ * @letters: complete
+ *
+ * Return: ssize_t
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t stat;
+	ssize_t write_stat;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters);
+
+	if (buf == NULL)
+	{
+		free(buf);
+		return (0);
+	}
+
+	stat = read(fd, buf, letters);
+
+	if (stat == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	write_stat = write(STDOUT_FILENO, buf, stat);
+
+	if (write_stat == -1 || write_stat < stat)
+	{
+		free(buf);
+		return (0);
+	}
+
+	return (write_stat);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN
+#define MAIN
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
