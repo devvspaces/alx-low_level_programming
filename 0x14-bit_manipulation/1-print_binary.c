605b65cfbe63c67fa4d22ce9942b50a6adf6c3a2
Netrobe Ubutu <sketcherslodge@gmail.com>
2023-04-08 01:25:04 +0100

    completed task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary
+ * representation of a number.
+ *
+ * @n: number
+ */
+void print_binary(unsigned long int n)
+{
+	int i = sizeof(int) * 7;
+	int s = 0;
+
+	for (; i >= 0; i--)
+		if (((n & (1 << i)) >> i) == 1)
+		{
+			s = 1;
+			printf("1");
+		}
+		else if (s == 1)
+			printf("0");
+
+	if (s == 0)
+		printf("0");
+}
