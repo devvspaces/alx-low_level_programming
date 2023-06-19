69f9f90d329e203e9e2974c44e853baad3bb50d7
Netrobe Ubuntu <netrobepy@gmail.com>
2023-06-19 17:14:27 +0100

    created dynamic library

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,28 +14,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i, j, s1_length, s2_length;
 
-/*Check if the strings passed are null*/
+	/*Check if the strings passed are null*/
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-/*Compute the length of the strings*/
+	/*Compute the length of the strings*/
 
 	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
 		;
 
 	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
 		;
-/*Memory reservation-for case 1 & 2.*/
+	/*Memory reservation-for case 1 & 2.*/
 	str = malloc(s1_length + n + 1);
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-/*Copy first string into str.*/
+	/*Copy first string into str.*/
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
-/*copy second stringg into str.*/
+	/*copy second stringg into str.*/
 	for (j = 0; j < n; j++)
 	{
 		str[i] = s2[j];
@@ -45,4 +45,3 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str[i] = '\0';
 	return (str);
 }
-
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,3 @@
+# Dynamic Libraries
+
+The commands, nm, ar, ranlib, ldconfig, ldd, are used to create, manipulate and manage the dynamic libraries.
