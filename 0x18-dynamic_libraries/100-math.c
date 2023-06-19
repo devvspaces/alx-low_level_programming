8d00592f93f8970504627d3b1925000e971a3b80
Netrobe Ubuntu <netrobepy@gmail.com>
2023-06-19 17:49:29 +0100

    created dll for python

---
+++ 0x18-dynamic_libraries/100-math.c
@@ -0,0 +1,66 @@
+
+
+/**
+ * add - add a and b
+ *
+ * @a: a
+ * @b: b
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub a and b
+ *
+ * @a: a
+ * @b: b
+ *
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul a and b
+ *
+ * @a: a
+ * @b: b
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div a and b
+ *
+ * @a: a
+ * @b: b
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod a and b
+ *
+ * @a: a
+ * @b: b
+ *
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
