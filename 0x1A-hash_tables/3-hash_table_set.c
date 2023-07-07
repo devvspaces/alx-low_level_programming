5403dcddc7e6712db3ddeab6cb2020423437c5c2
Netrobe Ubuntu <netrobepy@gmail.com>
2023-07-07 05:05:07 +0100

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,50 +4,44 @@
  * hash_table_set - Add or update an element in a hash table.
  * 
  * @ht: A pointer to the hash table.
- * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
+ * @value: The value of the key.
+ * @key: The key to add.
  *
  * Return: Upon failure - 0. else - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;
+	hash_node_t *new_hash_node;
+	char *dup_value;
 	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	dup_value = strdup(value);
+	if (dup_value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
-	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			ht->array[i]->value = dup_value;
 			return (1);
 		}
-	}
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(value_copy);
-		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht->array[index];
-	ht->array[index] = new;
+	new_hash_node = malloc(sizeof(hash_node_t));
+	if (new_hash_node == NULL)
+		return (free(dup_value), 0);
+
+	new_hash_node->key = strdup(key);
+	if (new_hash_node->key == NULL)
+		return (free(new_hash_node), 0);
+
+	new_hash_node->value = dup_value;
+	new_hash_node->next = ht->array[index];
+	ht->array[index] = new_hash_node;
 
 	return (1);
 }
